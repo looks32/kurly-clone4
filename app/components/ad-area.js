cc666973090f2054c967197b6791d8db63a9668c
Dang <56210432+looks32@users.noreply.github.com>
2024-05-17 13:55:10 +0900

    0517
    0517

--- app/components/ad-area.js
+++ app/components/ad-area.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function AdArea(props) {
   return (
     <div className="ad_area">
-      <img src={props.adImg} alt="광고 이미지" />
+      <Link href="#none;">
+        <img src={props.adImg} alt="광고 이미지" />
+      </Link>
     </div>
     )
 }
