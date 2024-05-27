6401ccb74d84c62708455dc1d6b366082d87e183
Dang <56210432+looks32@users.noreply.github.com>
2024-05-27 10:03:40 +0900

    0527
    0527

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -12,39 +12,39 @@ export default function Join() {
                 <li>
                     <label htmlFor="joinId">아이디</label>
                     <div>
-                        <input type="text" id="joinId" />
+                        <input type="text" id="joinId" placeholder="아이디를 입력해주세요" />
                         <Btn tit="중복확인" color="white"/>
                     </div>
                 </li>
                 <li>
                     <label htmlFor="joinPw">비밀번호</label>
                     <div>
-                        <input type="password" id="joinPw" />
+                        <input type="password" id="joinPw" placeholder="비밀번호를 입력해주세요"/>
                     </div>
                 </li>
                 <li>
                     <label htmlFor="joinPw2">비밀번호</label>
                     <div>
-                        <input type="password" id="joinPw2" />
+                        <input type="password" id="joinPw2" placeholder="비밀번호를 한번 더 입력해주세요"/>
                     </div>
                 </li>
                 <li>
                     <label htmlFor="joinName">이름</label>
                     <div>
-                        <input type="text" id="joinName" />
+                        <input type="text" id="joinName" placeholder="이름을 입력해 주세요" />
                     </div>
                 </li>
                 <li>
                     <label htmlFor="joinMail">이메일</label>
                     <div>
-                        <input type="text" id="joinMail" />
+                        <input type="text" id="joinMail" placeholder="예: maketkurly@kurly.com" />
                         <Btn tit="중복확인" color="white"/>
                     </div>
                 </li>
                 <li>
                     <label htmlFor="joinTel">휴대폰</label>
                     <div>
-                        <input type="text" id="joinTel" />
+                        <input type="text" id="joinTel" placeholder="숫자만 입력해주세요" />
                     </div>
                 </li>
             </ul>
