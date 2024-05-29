956af9f080c44eb9534efadc1b7869ba23121c19
Dang <56210432+looks32@users.noreply.github.com>
2024-05-30 00:25:46 +0900

    zod 수정
    zod 수정

--- app/join/actions.ts
+++ app/join/actions.ts
@@ -11,6 +11,11 @@ const checkJoinId = (joinId:string) => !joinId.includes('potato');
 
 const checkPasswords = ({joinPw, joinPw2} : {joinPw:string, joinPw2:string}) => joinPw === joinPw2
 
+// 소문자, 대문자, 숫자, 특수문자가 포함되어 있는지 확인하는 정규식
+const passwordRegex = new RegExp(
+	/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
 // username 은 string이어야하고 5~10글자 여야한다.
 //const joinNameSchema = z.string().min(5).max(10);
 
@@ -18,14 +23,14 @@ const formSchema = z.object({
 	joinId : z.string({
 		invalid_type_error : "숫자만 입력하지 말아주세요.",
 		required_error : "id를 입력해주세요."
-	}).min(3 ,"넘 짧셈").max(10, "넘 길으셈").refine(checkJoinId, "no potatoes!!!"),
-	joinPw  : z.string().min(3).max(10),
+	}).min(3 ,"넘 짧셈").max(10, "넘 길으셈").refine(checkJoinId, "no potatoes!!!").transform((joinId) => `😍 ${joinId} 😍`),
+	joinPw  : z.string().min(3).max(10).regex(passwordRegex, "비밀번호는 소문자, 대문자, 숫자, 특수문자를 포함해야합니다."),
 	joinPw2 : z.string().min(3).max(10),
 	joinName : z.string().min(3).max(10),
-	joinMail : z.string().email(),
+	joinMail : z.string().toLowerCase().email("이메일 형식에 맞게 입력해주세요."),
 	joinTel : z.string().min(3).max(10)
 }).refine( checkPasswords , {
-	message : '둘이 틀린디',
+	message : '동일한 비밀번호를 입력해주세요.',
 	path : ['joinPw2']
 })
 
@@ -54,7 +59,7 @@ export async function createAcccount(prevState:any, formData:FormData){
 
 		return result.error.flatten(); // error를 return 해줌.
 	} else {
-		console.log('회원가입 완료!')
+		console.log('회원가입 완료!', result.data)
 	}
 }
 
