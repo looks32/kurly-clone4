3b344cc0482d525d4a18aadd2b4f1e33e9c9aad7
Dang <56210432+looks32@users.noreply.github.com>
2024-05-31 10:52:09 +0900

    members 추가
    members 추가

---
+++ app/members/page.tsx
@@ -0,0 +1,35 @@
+import db from "../lib/db";
+
+async function getUsers() {
+  const user = await db.user.findMany({
+    select: {
+      userid: true,
+      password : true,
+      username: true,
+      email: true,
+      phone: true,
+      created_at: true,
+      updated_at: true
+    },
+  });
+  return user;
+}
+
+export default async function Profile() {
+  const user = await getUsers();
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {user.map((u) => (
+        <div key={u.userid}>
+          {`ID : ${u.userid}`}<br/>
+          {`PW : ${u.password}`}<br/>
+          {`NAME : ${u.username}`}<br/>
+          {`EMAIL : ${u.email}`}<br/>
+          {`PHONE : ${u.phone}`}<br/>
+          {`CREATE : ${u.created_at}`}<br/>
+          {`UPDATE : ${u.updated_at}`}<br/><br/>
+        </div>
+      ))}
+    </div>
+  );
+}
