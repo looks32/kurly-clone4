3d78d753e21d1bac313e925ac4eb4d4b43320c76
Dang <56210432+looks32@users.noreply.github.com>
2024-06-11 16:22:44 +0900

    검색 완료
    검색 완료

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -18,19 +18,26 @@ export default function Search() {
   }, [searchParams]);
 
   return (
-    <div>
+    <div className={styles.search_wrap}>
       <div className={styles.result}>'<strong>{name}</strong>'에 대한 검색결과</div>
-
-      {goods && (name && goods.some(slide => slide.text.includes(name)) ? goods.filter(slide => slide.text.includes(name)).map(slide => (
-      <div key={slide.id}>          
-        <Goods id={slide.id} poster={slide.poster} text={slide.text} before={slide.before} per={slide.per} price={slide.price} review={slide.review}/>
-      </div>
-    )) :
-    <p>검색된 상품이 없습니다.<br/>다른 검색어를 입력해주세요.</p>
-  )
-}
-
-
+      {goods && (name && goods.some(p => p.text.includes(name)) ? (
+        <>
+          <div>총 {goods.filter(p => p.text.includes(name)).length}건</div>
+          <ul>
+            {goods
+              .filter(p => p.text.includes(name))
+              .map(p => (
+                <li key={p.id}>          
+                  <Goods id={p.id} poster={p.poster} text={p.text} before={p.before} per={p.per} price={p.price} review={p.review}/>
+                </li>
+              ))}
+          </ul>
+        </>
+      ) : (
+        <div className={styles.no_data}>
+          검색된 상품이 없습니다.<br/>다른 검색어를 입력해주세요.
+        </div>
+      ))}
     </div>
   );
 }
